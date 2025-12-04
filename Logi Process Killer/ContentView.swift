afd79a1c0475dbc2f143d9f31c110109c795a66b
locolin1204 <locolin99@gmail.com>
2025-12-05 01:24:50 +0800

    Initial Commit

---
+++ Logi Process Killer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Logi Process Killer
+//
+//  Created by Lo Hoa Tsun Colin on 5/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Logi Process Killer/Logi_Process_KillerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Logi_Process_KillerApp.swift
+//  Logi Process Killer
+//
+//  Created by Lo Hoa Tsun Colin on 5/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Logi_Process_KillerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
